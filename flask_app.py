014dd1a87aee40a9b1089b26e3df94bf8f155dad
foamyguyls <foamyguy@gmail.com>
2014-12-13 20:54:19 +0000

    initial commit

---
+++ flask_app.py
@@ -0,0 +1,31 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, redirect
+import json, requests
+import random
+
+
+app = Flask(__name__)
+
+def get_links():
+    url = 'http://www.reddit.com/r/aww.json'
+    resp = requests.get(url=url)
+    data = json.loads(resp.text)
+    raw_image_links = []
+
+    for post in data['data']['children']:
+        ending = post['data']['url'][-4:]
+        if ending.lower() == ".jpg" or ending.lower() == ".png":
+            raw_image_links.append(post['data']['url'])
+
+    return raw_image_links
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    links = get_links()
+    print (links)
+    return redirect(random.choice(links), code=302)
